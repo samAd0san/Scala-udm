753da9adcb694a13fe2f8ae279b668315f2411dd
samAd0san <sohaibsamad.hala@gmail.com>
2024-09-18 17:44:55 +0530

    ToDos List v4 Added Error handling Code Refactor

--- src/main/scala/Revise/ToDosList.scala
+++ src/main/scala/Revise/ToDosList.scala
@@ -9,54 +9,71 @@ object ToDosList {
     var tasks = ListBuffer[Task]()
 
     def addTask(id: Int, description: String, isComplete: Boolean): Unit = {
-      val task = Task(id, description, isComplete)
-      tasks += task
-      println(s"Added task $task")
+      try {
+        if (id <= 0) throw new IllegalArgumentException("ID must be positive")
+        if (description.isEmpty) throw new IllegalArgumentException("Enter the Description")
+        val task = Task(id, description, isComplete)
+        tasks += task
+        println(s"Added task $task")
+      } catch {
+        case e: IllegalArgumentException => println(s"Error Adding Task: ${e.getMessage}")
+      }
     }
 
     def removeTask(id: Int): Unit = {
-      val taskToRemove = tasks.find(_.id == id)
-      tasks = tasks.filterNot(_.id == id)
-      taskToRemove match {
-        case Some(task) => println(s"Task Removed: $task")
-        case None => println(s"Task not found with id: $id")
+      try {
+        val taskToRemove = tasks.find(_.id == id)
+        if (taskToRemove.isEmpty) throw new NoSuchElementException(s"Task Not Found with id: $id")
+        tasks = tasks.filterNot(_.id == id)
+        println(s"Task Removed: $taskToRemove")
+      } catch {
+        case e: NoSuchElementException => println(s"Error removing Task: ${e.getMessage}")
       }
     }
 
     def removeAll(): Unit = {
-      tasks.clear()
-      println("All the tasks have been removed")
+      try {
+        if (tasks.isEmpty) throw new NoSuchElementException("No Tasks to Remove")
+        tasks.clear()
+        println("All the tasks have been removed")
+      } catch {
+        case e: NoSuchElementException => println(s"Error removing all tasks: ${e.getMessage}")
+      }
     }
 
     def updateTask(id: Int, newDescription: String, newIsComplete: Boolean): Unit = {
-      val index = tasks.indexWhere(_.id == id)
-      if (index != -1) {
+      try {
+        val index = tasks.indexWhere(_.id == id)
+        if (index == -1) throw new NoSuchElementException(s"No Task found with id: $id")
         val oldTask = tasks(index)
         tasks.update(index, Task(id, newDescription, newIsComplete))
         println(s"Task Updated From $oldTask --> ${tasks(index)} ")
-      } else {
-        println(s"No task found with id: $id")
+      } catch {
+        case e: NoSuchElementException => println(s"Error Updating Task: ${e.getMessage}")
       }
     }
 
     def updatePartially(id: Int, newDescription: Option[String] = None, newIsComplete: Option[Boolean] = None): Unit = {
-      val index = tasks.indexWhere(_.id == id)
-      if (index != -1) {
+      try {
+        val index = tasks.indexWhere(_.id == id)
+        if (index == -1) throw new NoSuchElementException(s"No Task found with id: $id")
         val oldTask = tasks(index)
         tasks.update(index, oldTask.copy(
           description = newDescription.getOrElse(oldTask.description),
           isComplete = newIsComplete.getOrElse(oldTask.isComplete)
         ))
         println(s"Task Updated From $oldTask --> ${tasks(index)} ")
-      }
+      } catch
+        case e: NoSuchElementException => println(s"Error partially updating: ${e.getMessage}")
     }
 
     def displayTask(): Unit = {
-      if (tasks.isEmpty) {
-        println("There are no tasks")
-      } else {
+      try {
+        if (tasks.isEmpty) throw new NoSuchElementException("There are no Tasks")
         println("Printing all the tasks")
         tasks.foreach(println)
+      } catch {
+        case e: NoSuchElementException => println(s"Error Displaying message: ${e.getMessage}")
       }
     }
   }
@@ -75,5 +92,10 @@ object ToDosList {
     TaskManager.displayTask()
     TaskManager.removeAll()
     TaskManager.displayTask()
+
+    TaskManager.addTask(-3, description = "Go to pray", isComplete = false)
+    TaskManager.addTask(5,description = "", false)
+
+    TaskManager.removeTask(29)
   }
 }
